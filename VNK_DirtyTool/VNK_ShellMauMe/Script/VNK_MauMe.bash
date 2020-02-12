da62696b5615c062dc8206f09d1aa36c5fc2ba10
Loi Huynh <hvloi@vinaknowledge.com>
2020-02-12 23:16:51 +0700

    vnkmm: Add scripts
    Branch: vnk/vnkccpp/vnksmm/add_smm/main_branch

---
+++ VNK_DirtyTool/VNK_ShellMauMe/Script/VNK_MauMe.bash
@@ -0,0 +1,96 @@
+################################################################################
+################################################################################
+##                                                                            ##
+##                  [ V N K  -  V I N A  K N O W L E D G E ]                  ##
+##                                                                            ##
+##                   SITE  : https://www.vinaknowledge.com                    ##
+##                   EMAIL : hvloi@vinaknowledge.com                          ##
+##                                                                            ##
+##                        TAN BINH DST, HCMC, VIET NAM                        ##
+##                                                                            ##
+##                 Copyright © 2019-2020 VNK - Vina Knowledge                 ##
+##                                                                            ##
+################################################################################
+################################################################################
+
+#!/bin/bash
+
+##
+# Shell script should be added the extension .bash, to let IDE recognize the
+# format of the file.
+##
+
+##
+# V N K  -  S C R I P T  M A` U  M E`
+##
+
+##------------------------------------------------------------------------------
+# D E F I N I T I O N S
+##
+
+##------------------------------------------------------------------------------
+# M A I N  C O D E S
+##
+
+# Reusing /etc/motd #
+cat $MOTD
+
+# Branding Access #
+echo -e  "[[BRANDING ACCESS,.......]]\n"
+echo -ne "[|                        ]\r"
+sleep 0.1
+echo -ne "[||                       ]\r"
+sleep 0.1
+echo -ne "[|||                      ]\r"
+sleep 0.1
+echo -ne "[||||                     ]\r"
+sleep 0.1
+echo -ne "[|||||                    ]\r"
+sleep 0.1
+echo -ne "[||||||                   ]\r"
+sleep 0.1
+echo -ne "[|||||||                  ]\r"
+sleep 0.1
+echo -ne "[||||||||                 ]\r"
+sleep 0.1
+echo -ne "[|||||||||                ]\r"
+sleep 0.1
+echo -ne "[||||||||||               ]\r"
+sleep 0.1
+echo -ne "[|||||||||||              ]\r"
+sleep 0.1
+echo -ne "[||||||||||||             ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||            ]\r"
+sleep 0.1
+echo -ne "[||||||||||||||           ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||          ]\r"
+sleep 0.1
+echo -ne "[||||||||||||||||         ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||||        ]\r"
+sleep 0.1
+echo -ne "[||||||||||||||||||       ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||||||      ]\r"
+sleep 0.1
+echo -ne "[||||||||||||||||||||     ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||||||||    ]\r"
+sleep 0.1
+echo -ne "[||||||||||||||||||||||   ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||||||||||  ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||||||||||| ]\r"
+sleep 0.1
+echo -ne "[|||||||||||||||||||||||||]\r"
+sleep 0.1
+
+# New Line #
+echo -e "\n"
+
+##
+# E N D
+##------------------------------------------------------------------------------
